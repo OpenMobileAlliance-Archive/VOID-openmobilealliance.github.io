97f36b68b6fc672b52c41ee9f225675ecb849807
nathan-omaorg <ndixon@omaorg.org>
2016-07-19 18:06:31 +0100

    Simplified templates for index.html created

---
+++ Scripts/enablerController.js
@@ -0,0 +1,16 @@
+﻿omaProject = angular.module('omaProjectIndex', []);
+
+var jsonFile = '../releases.json';
+
+omaProject.controller('ReleaseController', function ($scope, $http) {
+    var $this = this;
+
+    $http.get(jsonFile)
+        .success(function (result) {
+            $scope.releaseList = result;
+        })
+        .error(function () {
+            //$scope.errorMessage = "<p>Error... " + textStatus + "        " + errorThrown + "</p><p>" + jqXHR.responseText + "</p>";
+            $scope.errorMessage = "Error loading JSON file - " + jsonFile + ": Please check the JSON data is correct";
+        });
+});
