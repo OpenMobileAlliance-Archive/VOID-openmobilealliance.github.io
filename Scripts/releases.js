cc510226699150ea00ffef1aeff8cd380a39c1cb
nathan-omaorg <ndixon@omaorg.org>
2016-07-21 18:35:28 +0100

    Created notes

---
+++ Scripts/releases.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+    var $notes = $('.release-note-phase');
+
+    function setNoteWrapperHeight() {
+        $notes.each(function (key, value) {
+            $(this).parent().css('min-height', $(this).height());
+        });
+    }
+
+    setNoteWrapperHeight();
+
+    $(window).on("resize", setNoteWrapperHeight);
+});
